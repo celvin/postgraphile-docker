22f141f43b4fe50efefd887860c565e133509349
Celvin Rivas <josecelvin@gmail.com>
2018-06-14 00:26:56 -0600

    Entrypoint fix

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -2,18 +2,9 @@
 
 set -e
 
-#rm -rf /plugins/connection-filter/
-
-if [ ! -d "/plugins/connection-filter/node_modules" ]; then
-  cd /plugins/connection-filter/ && yarn install
-fi
-
 # /wait && postgraphile -n 0.0.0.0 --cors -c postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$PGHOST:$DB_PORT/$POSTGRES_DB -o -a -j -M --append-plugins /plugins/connection-filter/index.js
-
 #/wait && npx postgraphile -n 0.0.0.0 --cors -c postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$PGHOST/$POSTGRES_DB --schema $PG_SCHEMA
 
-node --inspect /node_modules/.bin/postgraphile -i / -n 0.0.0.0 --port 80 -c postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$PGHOST:$DB_PORT/$POSTGRES_DB -o -a -j -M --append-plugins /plugins/connection-filter/index.js
-
-#/bin/bash
+node --inspect /node_modules/.bin/postgraphile -i / -n $GRAPHILE_LISTEN --port $GRAPHILE_LISTEN_PORT -c postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DATABASE $GRAPHILE_EXTRAS
 
 exec "$@"
